1ea1a42b72bd50e877148ef9a3656a1d3a068cf7
PJ Gray <pj@evertrue.com>
2013-01-24 12:14:14 -0500

    app id changes - not finished

--- lightman.js
+++ lightman.js
@@ -2,16 +2,19 @@ var Spooky = require('spooky');
 var program = require('commander');
 
 program.version('0.0.1')
-  .usage('[options] <createDevCert|createDistCert>')
+  .usage('[options] <createDevCert|createDistCert|createAppID>')
   .option('-p, --password <password>', 'Use password [blah]')
   .option('-u, --username <username>', 'Use username [blah]')
   .option('-t, --team [team]', 'Use team')
   .option('-c, --csr [csr file]', 'Use csr file')
+  .option('-bn, --bundlename [bundle name]', 'Use bundle name')
+  .option('-bi, --bundleid [bundle id]', 'Use bundle id')
   .parse(process.argv);
 
 var world = require('./lib/world').World;
 var certificates = require('./lib/certificates');
 var authentication = require('./lib/authentication');
+var appids = require('./lib/appids');
 
 world(function(world) {
 
@@ -22,6 +25,10 @@ world(function(world) {
     certificates.createDevelopmentCertificate(program.csr);    
   } else if (program.args[0] == "createDistCert") {
     certificates.createDistrobutionCertificate(program.csr);    
+  } else if (program.args[0] == "createAppID") {
+    appids.createNewAppID(program.bundlename,program.bundleid);
+  } else if (program.args[0] == "listAppIDs") {
+    appids.listAppIDs();
   }
   
   // // say cheeze!
